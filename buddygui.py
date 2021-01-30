4e0bd6b1b6b4d9b9991e34cd906ab2cca6862b2d
David Lawson <39196767+linuxlawson@users.noreply.github.com>
2021-01-30 15:14:57 -0500

    added menu
    not much of one but it does include a handy 'clear fields' button

--- buddygui.py
+++ buddygui.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
-# Budget Buddy (GUI Version)
-# Income/Expenses Program
-# Written by David Lawson
+#Budget Buddy (GUI Version)
+#A Money Management Tool
+#Written by David Lawson
 
-#Uses grid manager, if editing
-#pay attention to row/column #'s
 
 try:
     import Tkinter as tk
@@ -21,18 +19,18 @@ root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 
-
 #Main Frame
-mainframe = tk.Frame(root, bd=0, highlightbackground="black", 
-                    highlightcolor="black", 
-                    highlightthickness=2, relief='flat')
+mainframe = tk.Frame(root, highlightcolor="#4D4D4D", 
+                    highlightbackground="#1A1A1A", 
+                    highlightthickness=1, 
+                    relief='flat')
 mainframe.grid(sticky='n')
 
 
 #labels
-lab = tk.Label(mainframe, text="\nBudget Buddy")
+lab = tk.Label(mainframe, text="\nBudget Buddy", font='Arial 10 bold')
 lab.grid(row=0, column=0, sticky='nw', padx=12, pady=4)
-lab1 = tk.Label(mainframe, text="Income and Expenses Program\n\n")
+lab1 = tk.Label(mainframe, text="A Money Management Tool\n\n")
 lab1.grid(row=1, column=0, sticky='nw', padx=12, pady=4)
 lab2 = tk.Label(mainframe, text="Enter Income & Expenses", fg='#BA0F0F')
 lab2.grid(row=2, column=0, sticky='nw', padx=12, pady=4)
@@ -60,10 +58,89 @@ def new_Remains():
 newRemains=tk.StringVar();
 
 
-def banked():
-    ban = tk.Label(mainframe, text="", fg="#B3B3B3")
-    ban.grid(row=15, column=2, sticky='nw', padx=0, pady=4)
-    ban = a14.get()
+def clear():
+	inca.delete('0', 'end')
+	a1.delete('0', 'end')
+	a2.delete('0', 'end')
+	a3.delete('0', 'end')
+	a4.delete('0', 'end')
+	a5.delete('0', 'end')
+	a6.delete('0', 'end')
+	a7.delete('0', 'end')
+	a8.delete('0', 'end')
+	a9.delete('0', 'end')
+	a10.delete('0', 'end')
+	a12.delete('0', 'end')
+	a14.delete('0', 'end')
+	a15.delete('0', 'end')
+	a16.delete('0', 'end')
+
+
+def clear_fields():
+    myTotal.set("")
+    myRemains.set("")
+    newRemains.set("")
+
+
+#file menu
+def exit_com(event=None):
+    win = tk.Toplevel()
+    win.title("Exit")
+    xit = tk.Label(win, text="\n\nAre you sure you want to exit?\n")
+    xit.pack()
+    ex = tk.Button(win, text="Exit", width=4, command=root.destroy)
+    ex.pack(side='left', padx=28, pady=4)
+    ex.focus_set()
+    ex.bind("<Return>", (lambda event: root.destroy()))
+    can = tk.Button(win, text="Cancel", width=4, command=win.destroy)
+    can.pack(side='right', padx=28, pady=4)
+    win.transient(root)
+    win.geometry('240x120')
+    win.wait_window()
+    
+
+#help menu
+def about_com(event=None):
+    win = tk.Toplevel()
+    win.title("About")
+    bout = tk.Label(win, text="""\n\nBudget Buddy
+    \nA Money Management Tool
+    \nCalculates Income & Expenses
+    \n\nMade in Python\n\n""")
+    bout.pack()
+    clo = tk.Button(win, text="Close", width=4, command=win.destroy)
+    clo.pack(padx=8, pady=4)
+    win.transient(root)
+    win.geometry('300x220+638+298')
+    win.wait_window()
+
+
+def trouble_com(event=None):
+    win = tk.Toplevel()
+    win.title("Troubleshooting")
+    trouble = tk.Label(win, justify='left', text="""\n\n
+This program will give a rough estimate of 
+how much you spend on a monthly basis.
+
+All entry fields must contain a number
+in order for program to work properly.
+
+Whole Numbers Only. If none, enter zero (0)\n
+
+Click on 'Total Costs' when you get there.
+This will get the Total Costs for the month,
+and the Remainder of your Monthly Income.
+
+Fill in the next two fields (if zero, enter 0)
+this will show New Remainder of income.
+\n\n""")
+    trouble.pack()
+    cls = tk.Button(win, text="Close", command=win.destroy)
+    cls.pack()
+    win.transient(root)
+    win.geometry('354x350+612+230')
+    win.wait_window()
+
 
 
 #income
@@ -72,6 +149,7 @@ incq.grid(row=3, column=0,  sticky='nw', padx=12, pady=4)
 inca = tk.Entry(mainframe, bd=1, width='8', justify='right')
 inca.grid(row=3, column=1, sticky='nw', padx=8, pady=4)
 income = inca.get()
+inca.focus_set()
 
 #rent
 q1 = tk.Label(mainframe, text="Rent:")
@@ -162,7 +240,7 @@ a14.grid(row=15, column=1, sticky='nw', padx=8, pady=4)
 savings = a14.get()
 
 #additional
-q15 = tk.Label(mainframe, text="List all additional earned income\n\n")
+q15 = tk.Label(mainframe, text="List all additional earned income:\n\n")
 q15.grid(row=16, column=0,  sticky='nw', padx=12, pady=4)
 a15 = tk.Entry(mainframe, bd=1,  width='8', justify='right')
 a15.grid(row=16, column=1, sticky='nw', padx=8, pady=4)
@@ -178,17 +256,40 @@ a16.config(state='readonly', bg='yellow')
 newremains = a16.get()
 
 spacer = tk.Label(mainframe, text="")
-spacer.grid(row=18, column=0,  sticky='nw', padx=12, pady=8)
+spacer.grid(row=18, column=3,  sticky='nw', padx=12, pady=8)
 
 
 #buttons (column 2)
 b1 = tk.Button(mainframe, text="Total Costs ", command=lambda: [getTotal(), getRemains()])
 b1.grid(row=13, column=2, sticky='nw', padx=28, pady=2)
 
-b2 = tk.Button(mainframe, text="Remainder ", command=lambda: [new_Remains(), banked()])
+b2 = tk.Button(mainframe, text="Remainder ", command=new_Remains)
 b2.grid(row=17, column=2, sticky='nw', padx=28, pady=2)
 
 
+
+#Menubar
+menu = tk.Menu(root, bd=1, relief='flat')
+root.config(menu=menu, bd=2)
+
+#Filemenu
+filemenu = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="File ", menu=filemenu)
+filemenu.add_command(label="Clear Fields", 
+                    command=lambda: [clear(), clear_fields()],
+                    accelerator="".rjust(2))
+filemenu.add_command(label="Exit", 
+                    command=exit_com, underline=1,
+                    accelerator="Ctrl+Q".rjust(2))                   
+
+
+#Helpmenu
+helpmenu = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="Help ", menu=helpmenu)
+helpmenu.add_command(label="About", command=about_com)
+helpmenu.add_command(label="Troubleshooting", command=trouble_com)
+
+root.bind_all('<Control-q>', exit_com)
 
 
 root.protocol("WM_DELETE_WINDOW")
